212e08e8ac76332e1f167bf8a95e6bb99026456e
TungAnh24 <nguyenhuutung02042001@gmail.com>
2022-08-24 13:42:18 +0700

    Cedar update Book entity

--- IcreCreamParlour.Model/Entities/Book.cs
+++ IcreCreamParlour.Model/Entities/Book.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Http;
 
 #nullable disable
 
@@ -32,8 +30,6 @@ namespace IcreCreamParlour.Model.Entities
         public int? IsDelete { get; set; }
 
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
-        [NotMapped]
-        [DisplayName("Upload File")]
         public IFormFile ImageFile { get; set; }
     }
 }
